0bb3d4987f840f3e94cf5fdea67c6441127beb6e
viktor ajayi <victorajayidamilare@gmail.com>
2024-11-20 01:08:23 +0100

    initialized the client and server socket connection

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const express = require('express')
-const {createServer} = require('http')
+const express = require('express');
+const { createServer } = require('http');
 const app = express();
 const server = createServer(app);
-const PORT = 3000
-const {join} = require('path')
+const PORT = 3000;
+const { join } = require('path');
+const { Server } = require('socket.io')
+const io = new Server(server)
 
-app.use('/', (req, res) => { 
-    res.sendFile(join(__dirname, 'index.html'));
+app.use('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
 })
+
 
 
 server.listen(PORT, () => {
-    // server listening
-    
-})
+  // server listening
+});
